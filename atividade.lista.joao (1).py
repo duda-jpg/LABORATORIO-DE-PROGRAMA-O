df7d614b373fe10ca1ac88e175132baedd02ca91
duda-jpg <meduardasgomes43@gmail.com>
2025-05-05 10:17:13 -0300

    Add files via upload

---
+++ atividade.lista.joao (1).py
@@ -0,0 +1,36 @@
+# 1° questão
+lista = []
+
+#2° questão
+lista.extend(["picanha", "cervejinha", "torcida"])
+
+#3° questão
+lista.insert(1, "pipos")
+
+#4° questão
+print(lista)
+print(f"o terceiro item da lista de compras é {lista[2]}")
+
+#5° questão
+print(f"\nJoão não precisa comprar este item: {lista[3]}")
+lista.remove("pipos")
+
+#6° questão
+print(f"lista atualizada = {lista}")
+print(f"João agora botou na cabeça que não vai comprar {lista[2]}")
+del lista [2]
+print(f"lista 100% atualizada = {lista}")
+
+#7° questão
+print(" João resolveu trocar a ", {lista[0]}, " pelo o abacate.\n")
+lista[0] = "abacate"
+print("lista atualizada final\n",lista)
+print(f"Aqui está a lista em ordem alfabetica\n")
+
+print(lista[0])
+print(lista[1])
+lista.sort
+
+#8° questão
+print(lista)
+
